a952261010aa24e00cac8e74bd42db2614386bd0
Danielton Paiva <53627217+danieltonpaiva@users.noreply.github.com>
2024-02-11 13:18:31 -0300

    dstgfdsg

--- facefusion/uis/components/source.py
+++ facefusion/uis/components/source.py
@@ -1,6 +1,6 @@
 from typing import Optional, List
 import gradio
-
+import os
 import facefusion.globals
 from facefusion import wording
 from facefusion.uis.typing import File
@@ -34,6 +34,12 @@ def render() -> None:
 		show_label = False
 	)
 	register_ui_component('source_image', SOURCE_IMAGE)
+	arquivos = [f for f in os.listdir('/kaggle/working/facenico6/exemplos') if os.path.isfile(os.path.join('/kaggle/working/facenico6/exemplos', f))]
+	files = []
+	for x in arquivos:
+		files.append('/kaggle/working/facenico6/exemplos/' + x)
+
+	examples = gradio.Examples(sorted(files), SOURCE_FILE, examples_per_page=20)
 
 
 def listen() -> None:
