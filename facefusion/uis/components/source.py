6991f6979301557ce78a4df40d0ae1f44529bf59
Danielton Paiva <53627217+danieltonpaiva@users.noreply.github.com>
2024-02-11 20:08:57 -0300

    Update source.py

--- facefusion/uis/components/source.py
+++ facefusion/uis/components/source.py
@@ -7,15 +7,31 @@ from facefusion.uis.typing import File
 from facefusion.filesystem import are_images
 from facefusion.uis.core import register_ui_component
 
-
+SOURCE_FILE : Optional[gradio.File] = None
 SOURCE_IMAGE : Optional[gradio.Image] = None
 
 
 def render() -> None:
+	global SOURCE_FILE
 	global SOURCE_IMAGE
 
+	are_source_images = are_images(facefusion.globals.source_paths)
+	SOURCE_FILE = gradio.File(
+		file_count = 'multiple',
+		file_types =
+		[
+			'.png',
+			'.jpg',
+			'.webp'
+		],
+		label = wording.get('source_file_label'),
+		value = facefusion.globals.source_paths if are_source_images else None
+	)
+	source_file_names = [ source_file_value['name'] for source_file_value in SOURCE_FILE.value ] if SOURCE_FILE.value else None
 	SOURCE_IMAGE = gradio.Image(
-		visible = True,
+		value = source_file_names[0] if are_source_images else None,
+		visible = are_source_images,
+		show_label = False
 	)
 	register_ui_component('source_image', SOURCE_IMAGE)
 	arquivos = [f for f in os.listdir('/kaggle/working/facenico6/exemplos') if os.path.isfile(os.path.join('/kaggle/working/facenico6/exemplos', f))]
@@ -23,14 +39,17 @@ def render() -> None:
 	for x in arquivos:
 		files.append('/kaggle/working/facenico6/exemplos/' + x)
 
-	examples = gradio.Examples(sorted(files), SOURCE_IMAGE, examples_per_page=20)
+	examples = gradio.Examples(sorted(files), SOURCE_FILE, examples_per_page=20)
 
 
 def listen() -> None:
-	SOURCE_IMAGE.change(update, inputs = SOURCE_IMAGE, outputs = SOURCE_IMAGE)
+	SOURCE_FILE.change(update, inputs = SOURCE_FILE, outputs = SOURCE_IMAGE)
 
 
-def update() -> gradio.Image:
-	facefusion.globals.source_paths = [SOURCE_IMAGE.value]
-	print(SOURCE_IMAGE.value)
-	return SOURCE_IMAGE
+def update(files : List[File]) -> gradio.Image:
+	file_names = [ file.name for file in files ] if files else None
+	if are_images(file_names):
+		facefusion.globals.source_paths = file_names
+		return gradio.Image(value = file_names[0], visible = True)
+	facefusion.globals.source_paths = None
+	return gradio.Image(value = None, visible = False)
