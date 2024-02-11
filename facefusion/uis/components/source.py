f2fa4319b8faf9c38c3f168ac2ba200dd98069c7
Danielton Paiva <53627217+danieltonpaiva@users.noreply.github.com>
2024-02-11 19:25:51 -0300

    ewtrt

--- facefusion/uis/components/source.py
+++ facefusion/uis/components/source.py
@@ -1,10 +1,10 @@
-from typing import Optional, List
+from typing import Any, IO, Optional
 import gradio
 import os
+
 import facefusion.globals
 from facefusion import wording
-from facefusion.uis.typing import File
-from facefusion.filesystem import are_images
+from facefusion.utilities import is_image
 from facefusion.uis.core import register_ui_component
 
 SOURCE_FILE : Optional[gradio.File] = None
@@ -15,9 +15,9 @@ def render() -> None:
 	global SOURCE_FILE
 	global SOURCE_IMAGE
 
-	are_source_images = are_images(facefusion.globals.source_paths)
+	is_source_image = is_image(facefusion.globals.source_path)
 	SOURCE_FILE = gradio.File(
-		file_count = 'multiple',
+		file_count = 'single',
 		file_types =
 		[
 			'.png',
@@ -25,19 +25,18 @@ def render() -> None:
 			'.webp'
 		],
 		label = wording.get('source_file_label'),
-		value = facefusion.globals.source_paths if are_source_images else None
+		value = facefusion.globals.source_path if is_source_image else None
 	)
-	source_file_names = [ source_file_value['name'] for source_file_value in SOURCE_FILE.value ] if SOURCE_FILE.value else None
 	SOURCE_IMAGE = gradio.Image(
-		value = source_file_names[0] if are_source_images else None,
-		visible = are_source_images,
+		value = SOURCE_FILE.value['name'] if is_source_image else None,
+		visible = is_source_image,
 		show_label = False
 	)
 	register_ui_component('source_image', SOURCE_IMAGE)
-	arquivos = [f for f in os.listdir('/kaggle/working/facenico6/exemplos') if os.path.isfile(os.path.join('/kaggle/working/facenico6/exemplos', f))]
+	arquivos = [f for f in os.listdir('/kaggle/working/facenico3/exemplos') if os.path.isfile(os.path.join('/kaggle/working/facenico3/exemplos', f))]
 	files = []
 	for x in arquivos:
-		files.append('/kaggle/working/facenico6/exemplos/' + x)
+		files.append('/kaggle/working/facenico3/exemplos/' + x)
 
 	examples = gradio.Examples(sorted(files), SOURCE_FILE, examples_per_page=20)
 
@@ -46,10 +45,9 @@ def listen() -> None:
 	SOURCE_FILE.change(update, inputs = SOURCE_FILE, outputs = SOURCE_IMAGE)
 
 
-def update(files : List[File]) -> gradio.Image:
-	file_names = [ file.name for file in files ] if files else None
-	if are_images(file_names):
-		facefusion.globals.source_paths = file_names
-		return gradio.Image(value = file_names[0], visible = True)
-	facefusion.globals.source_paths = None
+def update(file: IO[Any]) -> gradio.Image:
+	if file and is_image(file.name):
+		facefusion.globals.source_path = file.name
+		return gradio.Image(value = file.name, visible = True)
+	facefusion.globals.source_path = None
 	return gradio.Image(value = None, visible = False)
