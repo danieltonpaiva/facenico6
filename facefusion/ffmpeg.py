fcdef46c468db7816ba22c4b622b0cd9f89299c7
Danielton Paiva <53627217+danieltonpaiva@users.noreply.github.com>
2024-02-13 15:35:59 -0300

    Update ffmpeg.py

--- facefusion/ffmpeg.py
+++ facefusion/ffmpeg.py
@@ -11,6 +11,7 @@ from tqdm import tqdm
 
 def run_ffmpeg(input_command : List[str], desc="Processando video", target_path=""):
     frames = count_video_frame_total(target_path)
+    print("Total de frames: " + frames)
     commands = [ 'ffmpeg']
     commands.extend(input_command)
     # Cria um objeto tqdm para exibir a barra de progresso
@@ -87,7 +88,7 @@ def compress_image(output_path : str) -> bool:
 def merge_video(target_path : str, fps : float) -> bool:
 	temp_output_video_path = get_temp_output_video_path(target_path)
 	temp_frames_pattern = get_temp_frames_pattern(target_path, '%04d')
-	commands = [ '-hwaccel', 'auto', '-r', str(fps), '-i', temp_frames_pattern, '-c:v', facefusion.globals.output_video_encoder ]
+	commands = [ '-hwaccel', 'auto', '-ignore_loop', '0', '-r', str(fps), '-i', temp_frames_pattern, '-c:v', facefusion.globals.output_video_encoder ]
 	if facefusion.globals.output_video_encoder in [ 'libx264', 'libx265' ]:
 		output_video_compression = round(51 - (facefusion.globals.output_video_quality * 0.51))
 		commands.extend([ '-crf', str(output_video_compression) ])
